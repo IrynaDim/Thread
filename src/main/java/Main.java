fd1f118c3842ba3bd6a713d0b5f523a3ad73eaf2
i.dymytreyeva <i.dymytreyeva@gmail.com>
2022-06-23 10:05:13 +0300

    some message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
+
 public class Main {
     public static void main(String[] args) {
-        Counter count = new Counter();
-        MyThread myThread = new MyThread(count);
-        MyThreadRunnable myThreadRunnable = new MyThreadRunnable(count);
-        Thread thread = new Thread(myThreadRunnable);
-        thread.start();
-        myThread.start();
+
     }
 }
